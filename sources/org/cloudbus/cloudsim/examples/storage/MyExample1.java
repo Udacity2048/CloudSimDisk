e2704178cddd2701cce5f89a4be3aa1b20b15211
Baptiste <baplou-4@student.ltu.se>
2015-04-06 16:22:03 +0200

    New: Basic example 1 + MyBasicDistri which is used to read information from a file in case of Basic example.

--- sources/org/cloudbus/cloudsim/examples/storage/MyExample1.java
+++ sources/org/cloudbus/cloudsim/examples/storage/MyExample1.java
@@ -1,12 +1,15 @@
 package org.cloudbus.cloudsim.examples.storage;
 
 /**
- * A basic example with 3 requests in a time interval of 1 second.
+ * Example 1: this example aims to understand the basic operation of storage
+ * example. In this scenario, 3 requests are sent to the Datacenter respectively
+ * at 0.2, 0.4 and 0.7 second(s). Each requests adds respectively FileA(1MB),
+ * FileB(2MB) and FileC(3MB) on the persistent storage. No files are retrieved.
  * 
  * @author Baptiste Louis
  */
 public class MyExample1 {
-	
+
 	/**
 	 * The main method.
 	 * 
@@ -14,27 +17,23 @@ public class MyExample1 {
 	 *            the arguments
 	 * @throws Exception
 	 */
-	public static void main(
-			String[] args) throws Exception {
-		
+	public static void main(String[] args) throws Exception {
+
 		// Parameters
-		String name = "Basic test example"; // name of the simulation
-		String type = "unif"; // type of the workload
+		String name = "Basic Example 1"; // name of the simulation
+		String type = "basic"; // type of the workload
 		int NumberOfRequest = 3; // Number of requests
-		String RequestArrivalDistri = ""; // No file source for time distribution
+		String RequestArrivalDistri = "basic/example1/ex1RequestArrivalDistri.txt"; // time
+																					// distribution
 		String requiredFiles = ""; // No files required
-		String dataFiles = "ex1DataFiles.txt"; // Hypothetical Wikipedia dataFiles
+		String dataFiles = "basic/example1/ex1DataFiles.txt"; // Hypothetical
+																// Wikipedia
+																// dataFiles
 		String startingFilesList = ""; // No files to start
-		
+
 		// Execution
-		new MyRunner(
-				name,
-				type,
-				NumberOfRequest,
-				RequestArrivalDistri,
-				requiredFiles,
-				dataFiles,
-				startingFilesList);
+		new MyRunner(name, type, NumberOfRequest, RequestArrivalDistri,
+				requiredFiles, dataFiles, startingFilesList);
 	}
-	
+
 }
--- sources/org/cloudbus/cloudsim/power/MyPowerDatacenterBroker.java
+++ sources/org/cloudbus/cloudsim/power/MyPowerDatacenterBroker.java
@@ -11,6 +11,7 @@ import org.cloudbus.cloudsim.core.CloudSim;
 import org.cloudbus.cloudsim.core.CloudSimTags;
 import org.cloudbus.cloudsim.distributions.ContinuousDistribution;
 import org.cloudbus.cloudsim.distributions.ExponentialDistr;
+import org.cloudbus.cloudsim.distributions.MyBasicDistr;
 import org.cloudbus.cloudsim.distributions.MyPoissonDistr;
 import org.cloudbus.cloudsim.distributions.MyWikiDistr;
 import org.cloudbus.cloudsim.distributions.UniformDistr;
@@ -23,18 +24,19 @@ import org.cloudbus.cloudsim.lists.VmList;
  * 
  */
 public class MyPowerDatacenterBroker extends PowerDatacenterBroker {
-	
+
 	/** History of requests (cloudlets) arrival rate. */
-	private List<Double>			History	= new ArrayList<Double>();
-	
+	private List<Double> History = new ArrayList<Double>();
+
 	/** Distribution used for arrival rate. */
-	private ContinuousDistribution	distri;
-	
+	private ContinuousDistribution distri;
+
 	/**
 	 * Created a new DatacenterBroker object.
 	 * 
 	 * @param name
-	 *            name to be associated with this entity (as required by Sim_entity class from simjava package)
+	 *            name to be associated with this entity (as required by
+	 *            Sim_entity class from simjava package)
 	 * @param type
 	 *            the distribution type
 	 * @param RequestArrivalDistri
@@ -42,83 +44,80 @@ public class MyPowerDatacenterBroker extends PowerDatacenterBroker {
 	 * @throws Exception
 	 *             the exception
 	 */
-	public MyPowerDatacenterBroker(
-			String name,
-			String type,
+	public MyPowerDatacenterBroker(String name, String type,
 			String RequestArrivalDistri) throws Exception {
-		super(
-				name);
-		
-		setDistri(type,
-				RequestArrivalDistri);
+		super(name);
+
+		setDistri(type, RequestArrivalDistri);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.cloudbus.cloudsim.DatacenterBroker#submitCloudlets()
 	 */
 	@SuppressWarnings("javadoc")
 	@Override
 	protected void submitCloudlets() {
-		
+
 		// Initialize local variable
 		int vmIndex = 0;
 		double tempDelay = 0;
-		
+
 		// For each Cloudlet of the Cloudlet list...
 		for (Cloudlet cloudlet : getCloudletList()) {
 			MyCloudlet myCloudlet = (MyCloudlet) cloudlet;
-			
+
 			// Vm binding check
 			Vm vm;
-			if (cloudlet.getVmId() == -1) { // if user didn't bind this cloudlet and it has not been
+			if (cloudlet.getVmId() == -1) { // if user didn't bind this cloudlet
+											// and it has not been
 											// executed yet
 				vm = getVmsCreatedList().get(vmIndex);
 			} else { // submit to the specific vm
-				vm = VmList.getById(getVmsCreatedList(),
-						cloudlet.getVmId());
+				vm = VmList.getById(getVmsCreatedList(), cloudlet.getVmId());
 				if (vm == null) { // vm was not created
-					Log.printLine(CloudSim.clock() + ": " + getName() + ": Postponing execution of cloudlet "
-							+ cloudlet.getCloudletId() + ": bount VM not available");
+					Log.printLine(CloudSim.clock() + ": " + getName()
+							+ ": Postponing execution of cloudlet "
+							+ cloudlet.getCloudletId()
+							+ ": bount VM not available");
 					continue;
 				}
 			}
-			
+
 			// set VM ID to the Cloudlet
 			cloudlet.setVmId(vm.getId());
-			
-			// request arrival rate sample according to a specific distribution type
+
+			// request arrival rate sample according to a specific distribution
+			// type
 			tempDelay = distri.sample();
-			
+
 			// store arrival time
 			History.add(tempDelay);
-			
-			// each Cloudlet are delayed according to the request arrival rate sample
-			send(getVmsToDatacentersMap().get(vm.getId()),
-					tempDelay,
-					CloudSimTags.CLOUDLET_SUBMIT,
-					myCloudlet);
-			Log.formatLine("%.1f: %s: Cloudlet #%3d is scheduled to be sent to VM #%3d in %7.3f second(s)",
-					CloudSim.clock(),
-					getName(),
-					cloudlet.getCloudletId(),
-					vm.getId(),
-					tempDelay);
-			
+
+			// each Cloudlet are delayed according to the request arrival rate
+			// sample
+			send(getVmsToDatacentersMap().get(vm.getId()), tempDelay,
+					CloudSimTags.CLOUDLET_SUBMIT, myCloudlet);
+			Log.formatLine(
+					"%.1f: %s: Cloudlet #%3d is scheduled to be sent to VM #%3d in %7.3f second(s)",
+					CloudSim.clock(), getName(), cloudlet.getCloudletId(),
+					vm.getId(), tempDelay);
+
 			// increment variable
 			cloudletsSubmitted++;
 			vmIndex = (vmIndex + 1) % getVmsCreatedList().size();
-			
+
 			// update Cloudlet Submitted List
 			getCloudletSubmittedList().add(cloudlet);
 		}
-		
+
 		// remove submitted cloudlets from waiting list
 		for (Cloudlet cloudlet : getCloudletSubmittedList()) {
 			getCloudletList().remove(cloudlet);
 		}
 	}
-	
+
 	/**
 	 * Gets the arrival time history
 	 * 
@@ -127,7 +126,7 @@ public class MyPowerDatacenterBroker extends PowerDatacenterBroker {
 	public List<Double> getArrivalTimeHistory() {
 		return History;
 	}
-	
+
 	/**
 	 * Sets the request arrival distribution
 	 * 
@@ -136,36 +135,30 @@ public class MyPowerDatacenterBroker extends PowerDatacenterBroker {
 	 * @param source
 	 *            the source file containing time distribution
 	 */
-	public void setDistri(
-			String type,
-			String source) {
+	public void setDistri(String type, String source) {
 		switch (type) {
-			case "expo":
-				distri = new ExponentialDistr(
-						60);
-				break;
-			
-			case "pois":
-				distri = new MyPoissonDistr(
-						60);
-				break;
-			
-			case "unif":
-				distri = new UniformDistr(
-						0,
-						1);
-				break;
-			
-			case "wiki":
-				distri = new MyWikiDistr(
-						source);
-				break;
-			
-			default:
-				distri = new UniformDistr(
-						1,
-						2);
-				break;
+		case "basic":
+			distri = new MyBasicDistr(source);
+			break;
+		case "expo":
+			distri = new ExponentialDistr(60);
+			break;
+
+		case "pois":
+			distri = new MyPoissonDistr(60);
+			break;
+
+		case "unif":
+			distri = new UniformDistr(0, 1);
+			break;
+
+		case "wiki":
+			distri = new MyWikiDistr(source);
+			break;
+
+		default:
+			distri = new UniformDistr(1, 2);
+			break;
 		}
 	}
 }
