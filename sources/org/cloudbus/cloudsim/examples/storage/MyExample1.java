9310fac1bcd49574df1b8cb38b7b7ac5f7fea927
Baptiste <baplou-4@student.ltu.se>
2015-04-13 18:37:13 +0200

    Update: MyExample1 updated. This example is more flexible because we can change the number of request between 1 to 9 (can be used for understanding or basic test).

--- sources/org/cloudbus/cloudsim/examples/storage/MyExample1.java
+++ sources/org/cloudbus/cloudsim/examples/storage/MyExample1.java
@@ -4,9 +4,9 @@ import org.cloudbus.cloudsim.power.models.harddrives.PowerModelHdd;
 import org.cloudbus.cloudsim.storage.models.harddrives.StorageModelHdd;
 
 /**
- * Example 1: this example aims to understand the basic operation of storage example. In this scenario, 3 requests are
- * sent to the Datacenter respectively at 0.2, 0.4 and 0.7 second(s). Each requests adds respectively FileA(1MB),
- * FileB(2MB) and FileC(3MB) on the persistent storage. No files are retrieved.
+ * Example 1: this example aims to understand the basic operation of storage example. In this scenario, 1 to 9 requests
+ * can be sent to the Datacenter respectively at 0.1, 0.2 ... 0.9 second(s). Each requests adds respectively FileA(1MB),
+ * FileB(2MB) ... FileI(9MB) on the persistent storage. No files are retrieved.
  * 
  * @author Baptiste Louis
  */
@@ -24,7 +24,7 @@ public class MyExample1 {
 		// Parameters
 		String name = "Basic Example 1"; // name of the simulation
 		String type = "basic"; // type of the workload
-		int NumberOfRequest = 3; // Number of requests
+		int NumberOfRequest = 9; // Number of requests
 		String RequestArrivalDistri = "basic/example1/ex1RequestArrivalDistri.txt"; // time distribution
 		String requiredFiles = ""; // No files required
 		String dataFiles = "basic/example1/ex1DataFiles.txt"; // dataFiles Names and Sizes
